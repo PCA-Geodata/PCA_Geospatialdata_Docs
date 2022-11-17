6008fbc8a9a6517bf826616bc11887ca87854279
PCA Geospatial Data <112809095+PCA-Geodata@users.noreply.github.com>
2022-11-17 22:09:51 +0000

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -34,7 +34,7 @@ html_theme = 'sphinx_rtd_theme'
 
 
 html_static_path = ['_static']
-html_logo = "docs/source/images/PCA logo round.jpg"
+html_logo = "images/PCA_logo_round.jpg"
 html_theme_options = {
     'logo_only': True,
     'display_version': False,
