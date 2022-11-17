7183d6ccf33e603aa81e8034b6f9d54880c32ed8
PCA Geospatial Data <112809095+PCA-Geodata@users.noreply.github.com>
2022-11-17 15:34:13 +0000

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,12 +2,12 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'PCA Geospatial Data Documentation'
+copyright = '2022, PCA'
+author = 'Pinna V.'
 
-release = '0.1'
-version = '0.1.0'
+release = '0.0.1'
+version = '0.0.1'
 
 # -- General configuration
 
